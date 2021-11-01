b2430070cd7220cf05a7d391f6748f064a60b886
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2021-11-01 15:47:36 -0400

    added js folder and scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// This is a comment
+/* This is a longer comment.
+It might explain, in detail, why we made a certain decision. Or, it could show examples about how to use the following code snippet. */
+
+alert('Hello World');
