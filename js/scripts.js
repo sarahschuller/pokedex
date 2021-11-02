5fe343b4a99709c56579329b52cf289c1fe3ad19
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2021-11-02 16:16:34 -0400

    Added pokemonList with several objects in the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-// This is a comment
-/* This is a longer comment.
-It might explain, in detail, why we made a certain decision. Or, it could show examples about how to use the following code snippet. */
-
-alert('Hello World');
-
-let favoriteFood = 'Chicken Tikka Masala';
-document.write(favoriteFood);
+let pokemonList= [
+  {name:'Typhlosion', height: 5.07, type: 'fire', abilities: 'Blaze'}
+  {name:'Gengar', height: 4.11, type: ['ghost', 'poison'], abilities: 'Cursed Body'}
+  {name:'Greninja', height: 4.11, type: ['water','dark'], abilities: 'Torrent'}
+  {name:'Umbreon', height: 3.03, type: 'dark', abilities: 'Synchronize'}
+  {name:'Luxray', height: 4.07, type: 'electric', abilities: ['Intimidate', 'Rivalry'] }
+  {name:'Ninetales', height: 3.07, type: 'fire', abilities: 'Flash Fire'}
+];
