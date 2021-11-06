79647812817029f7a9786e06f11575c0860f628b
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2021-11-06 19:06:47 -0400

    add for loop to display list of pokemon with descriptive text

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,57 @@
 let pokemonList= [
-  {name:'Typhlosion', height: 5.07, type: ['fire'], abilities: ['Blaze'] },
-  {name:'Gengar', height: 4.11, type: ['ghost', 'poison'], abilities: ['Cursed Body'] },
-  {name:'Greninja', height: 4.11, type: ['water','dark'], abilities: ['Torrent'] },
-  {name:'Umbreon', height: 3.03, type: ['dark'], abilities: ['Synchronize'] },
-  {name:'Luxray', height: 4.07, type: ['electric'], abilities: ['Intimidate', 'Rivalry'] },
-  {name:'Ninetales', height: 3.07, type: ['fire'], abilities: ['Flash Fire'] },
+  {
+    name:'Typhlosion',
+    height: 5.7,
+    type: ['fire'],
+    abilities: ['Blaze']
+   },
+  {
+    name:'Gengar',
+    height: 4.11,
+    type: ['ghost', 'poison'],
+    abilities: ['Cursed Body']
+    },
+  {
+    name:'Greninja',
+    height: 4.11,
+    type: ['water','dark'],
+    abilities: ['Torrent']
+   },
+  {
+    name:'Umbreon',
+    height: 3.3,
+    type: ['dark'],
+    abilities: ['Synchronize']
+   },
+  {
+    name:'Luxray',
+    height: 4.7,
+    type: ['electric'],
+    abilities: ['Intimidate', 'Rivalry']
+  },
+  {
+    name:'Ninetales',
+    height: 3.7,
+    type: ['fire'],
+    abilities: ['Flash Fire']
+  },
 ];
+
+let text = " ";
+for (let i = 0; i < pokemonList.length; i++){
+ if(pokemonList[i].height <5 && pokemonList[i].height >4){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>" + "<br>");
+}
+  else if (pokemonList[i].height > 5.5){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <i>Wow, that’s big!</i>" + "<br>" + "<br>");
+}
+  else if (pokemonList[i].height < 5){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <i>This pokemon is tiny!</i>" + "<br>" + "<br>");
+}
+  else {
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>" + "<br>");
+}
+}
