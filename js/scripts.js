545deb67b17cc63ad2992ee7f0e4dfa334a95663
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2021-11-30 17:13:12 -0500

    added function to create button that scrolls to the top of the page

--- js/scripts.js
+++ js/scripts.js
@@ -121,3 +121,20 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+mybutton = document.getElementById('myBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    mybutton.style.display='block';
+  }else{
+    mybutton.style.display = 'none';
+  }
+}
+
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
